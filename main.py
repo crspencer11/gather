e1ee0a51333b1457d09e4d53b99f07e584db4fc6
Lil Blick <chrispencer@berkeley.edu>
2026-03-25 12:56:09 -0700

    cleanup

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-home = 'https://coinmarketcap.com/'
-home_page = requests.get(home)
-soup = BeautifulSoup(home_page.text, 'html.parser')
+cmc = 'https://coinmarketcap.com/'
+# home_page = requests.get(cmc)
+# soup = BeautifulSoup(home_page.text, 'html.parser')
 
-# Example: Get all the titles of cryptocurrencies listed on the first page
-# for row in soup.find_all('tr'):
-#     name = row.find('p', class_='coin-item-symbol')
-#     if name:
-#         print(name.text)
+# # Example: Get all the titles of cryptocurrencies listed on the first page
+# # for row in soup.find_all('tr'):
+# #     name = row.find('p', class_='coin-item-symbol')
+# #     if name:
+# #         print(name.text)
 
-class DataClass:
-    def __init__(self, home: str):
-        self.home = home
-        self.total_pages = self._find_last_page(home)
+class DataSource:
+    def __init__(self, base_url: str):
+        self._base_url = base_url
+        self._home_page = self._create_home_page()
+        self._total_pages = self._find_last_page()
 
-    @classmethod
-    def _find_last_page(page) -> int:
+    def _create_home_page(self):
+        home = requests.get(self._base_url)
+        return BeautifulSoup(home.text, 'html.parser')
+
+    def _find_last_page(self) -> int:
         """
         Finds last page number from HTML webpage. this is to set Loop threshold for later processing.
         """
-        page_items = page.find_all('a', attrs={'aria-label': True})
+        page_items = self._home_page.find_all('a', attrs={'aria-label': True})
         if len(page_items) >= 2:
             end_page = page_items[-2]
             last_page_number = int(end_page.text)
@@ -33,9 +37,12 @@ class DataClass:
     def gather_data(self):
         print("Gathering Data...")
         pages = []
-        total_pages = self.find_last_page(home)
+        total_pages = self._total_pages
         for page_num in range(1, total_pages):
-            pages.append(home+"?page="+str(page_num))
+            pages.append(self._base_url+"?page="+str(page_num))
 
         for page in pages:
             print(f"Scrapping data from Page {page}")
+            current_page = requests.get(page)
+
+cmc = DataSource(cmc)
